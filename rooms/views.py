b2c2a7e783852a5b1f85bafae213f7cf5ccc93dc
unknown <dymaeng@dymaeng-office.intermico.net>
2021-05-27 11:21:22 +0900

    20210527-1

---
+++ rooms/views.py
@@ -0,0 +1,23 @@
+from math import ceil
+from django.shortcuts import render
+from . import models
+
+
+def all_rooms(request):
+    page = request.GET.get("page", 1)
+    page = int(page or 1)
+    page_size = 10
+    limit = page_size * page
+    offset = limit - page_size
+    page_count = ceil(models.Room.objects.all().count() / page_size)
+    all_rooms = models.Room.objects.all()[offset:limit]
+    return render(
+        request,
+        "rooms/home.html",
+        context={
+            "rooms": all_rooms,
+            "page": page,
+            "page_count": page_count,
+            "page_range": range(1, page_count + 1),
+        },
+    )
