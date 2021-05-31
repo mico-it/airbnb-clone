4981451653743aded6defabc66605adf7fe1470d
unknown <dymaeng@dymaeng-office.intermico.net>
2021-05-31 09:50:24 +0900

    20210531- #12 Detail View

--- rooms/views.py
+++ rooms/views.py
@@ -1,20 +1,23 @@
+from django.views.generic import ListView
+from django.urls import reverse
 from django.shortcuts import render, redirect
-from django.core.paginator import Paginator, EmptyPage
 from . import models
 
 
-def all_rooms(request):
-    page = request.GET.get("page", 1)
-    room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10, orphans=5)
+class HomeView(ListView):
 
+    """HomeView Definition"""
+
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 5
+    ordering = "price"
+    context_object_name = "rooms"
+
+
+def room_detail(request, pk):
     try:
-        rooms = paginator.page(page)
-        return render(
-            request,
-            "rooms/home.html",
-            context={"page": rooms},
-        )
-    except EmptyPage:
-        rooms = paginator.page(1)
-        return redirect("/")
+        room = models.Room.objects.get(pk=pk)
+        return render(request, "rooms/detail.html", {"room": room})
+    except models.Room.DoesNotExist:
+        return redirect(reverse("core:home"))
