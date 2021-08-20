fb949cc7ca8dd2cdead1ea8f562d79f5be55e505
unknown <dymaeng@dymaeng-office.intermico.net>
2021-08-20 16:02:15 +0900

    20210820

--- reservations/views.py
+++ reservations/views.py
@@ -4,6 +4,7 @@ from django.contrib import messages
 from django.shortcuts import render, redirect, reverse
 from django.views.generic import View
 from rooms import models as room_models
+from reviews import forms as review_forms
 from . import models
 
 
@@ -43,8 +44,11 @@ class ReservationDetailView(View):
         ):
             raise Http404()
 
+        form = review_forms.CreateReviewForm()
         return render(
-            self.request, "reservations/detail.html", {"reservation": reservation}
+            self.request,
+            "reservations/detail.html",
+            {"reservation": reservation, "form": form},
         )
 
 
