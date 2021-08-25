d7d8a425c86e3c02e9cb9b2efb105c06a5c51727
unknown <dymaeng@dymaeng-office.intermico.net>
2021-08-25 14:33:00 +0900

    20210825

--- config/settings.py
+++ config/settings.py
@@ -61,6 +61,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
 ]
 
 ROOT_URLCONF = "config.urls"
@@ -154,3 +155,6 @@ EMAIL_FROM = os.environ.get("GMAIL_USERNAME")
 
 # Auth
 LOGIN_URL = "/users/login/"
+
+# Locale
+LOCALE_PATHS = (os.path.join(BASE_DIR, "locale"),)
--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 import os
 import requests
+from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.auth import authenticate, login, logout
@@ -8,6 +9,7 @@ from django.contrib.auth.views import PasswordChangeView
 from django.core.files.base import ContentFile
 from django.views.generic import FormView, DetailView, UpdateView
 from django.urls import reverse_lazy
+from django.utils import translation
 from django.shortcuts import redirect, reverse
 from . import forms, models, mixins
 
@@ -276,3 +278,10 @@ def switch_hosting(request):
     except KeyError:
         request.session["is_hosting"] = True
     return redirect(reverse("core:home"))
+
+
+def switch_language(request):
+    lang = request.GET.get("lang", None)
+    if lang is not None:
+        request.session[translation.LANGUAGE_SESSION_KEY] = lang
+    return HttpResponse(status=200)
